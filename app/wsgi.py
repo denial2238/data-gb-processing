8c80c9667d19ea2374a7ca4b7a015fe523147257
denial2238 <91253346+denial2238@users.noreply.github.com>
2024-10-27 00:41:51 +0200

    Delete app directory

--- app/wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Set the path to your app directory
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
-
-from flask_app import create_app
-
-application = create_app()
