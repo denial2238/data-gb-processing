3fa7b72f1c0bb82e365dca26a6ffb5f2a8d1b317
denial2238 <91253346+denial2238@users.noreply.github.com>
2024-10-26 23:36:59 +0000

    fixedsssss

--- flask_app.py
+++ flask_app.py
@@ -165,4 +165,4 @@ def get_product_from_eshop(recommended_product_data, context, model):
     return response.text
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
